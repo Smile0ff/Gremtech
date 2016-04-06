76fac1d7cdf3c80bbb383f9844079ea254db79f1
Smile0ff <a.parneta@gmail.com>
2016-04-06 17:05:32 +0300

    main ready

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,11 +1,11 @@
 "use strict";
 
 import "jquery";
-//import Loader from "./lib/loader";
+import Loader from "./lib/loader";
 import Scrollify from "./lib/scrollify";
 import ScrollAppearance from "./lib/scrollAppearance";
 
-//new Loader();
+new Loader();
 
 $(() => {
 
